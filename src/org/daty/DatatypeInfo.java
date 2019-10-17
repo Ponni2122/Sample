4fcdea7e91cbcca28eaf68768c38959c74fba1e8
Ponni2122 <ponni.parthi@gmail.com>
2019-10-17 12:58:44 +0530

    Sample code

---
+++ src/org/daty/DatatypeInfo.java
@@ -0,0 +1,21 @@
+package org.daty;
+
+public class DatatypeInfo {
+	
+	public static void main (String[] args) {
+		byte b = 127;
+		short s = 3290;
+		int i = 789654;
+		long l = 4565765068562365l;
+		float f = 45643.21f;
+		double d = 546523133.54d;
+		char c = 'h';
+		String z = "Harshi";
+		boolean x = true;
+		System.out.println("short is :" +s);
+		System.out.println(x);
+		System.out.println("My Name is " +c);
+	
+	}
+
+}
